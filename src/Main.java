40dd17f549f7ba009822054f766bf3ec8c314ae8
Ferreyd <cordina.nicolas@gmail.com>
2014-10-07 14:39:22 +0200

    Ajout d'une méthode permettant de faire une requete simple Ajout d'une méthode permettant de vérifier que les données existent déjà

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 import java.sql.*;
 
 /**
  * Created by Nicolas on 03/10/2014.
  */
 
-public class Main {
+public class Main
+{
 
     Connection con;
     String nomBD = "test";
@@ -13,7 +16,8 @@ public class Main {
     String mdp = "1234";
     Statement stmt;
 
-    public Main(String nomBD, String url, String login, String mdp) throws Exception {
+    public Main(String nomBD, String url, String login, String mdp) throws Exception
+    {
         this.nomBD = nomBD;
         this.url = url;
         this.login = login;
@@ -27,10 +31,27 @@ public class Main {
         stmt = con.createStatement();
     }
 
+    public static void main(String[] args) throws Exception
+    {
+
+        String nomBD = "test";
+        String url = "jdbc:mysql://flatbrains.eu/" + nomBD;
+        String login = "NicoTPJDBC";
+        String mdp = "1234";
+
+        Main main = new Main(nomBD, url, login, mdp);
+        main.creerTable();
+        main.requetesSelectCafe();
+        main.close();
+
+
+    }
+
     /**
      * Print stdout some informations about the database connection
      */
-    private void printInfo() throws Exception {
+    private void printInfo() throws Exception
+    {
         // Get meta-data about the database
         DatabaseMetaData info = con.getMetaData();
         System.out.println("\nConnected to :\t" + info.getURL());
@@ -40,15 +61,16 @@ public class Main {
 
     /* Print stdout all pending SQLWarning warnings
     */
-    private boolean checkForSQLWarnings(SQLWarning w)
-            throws SQLException
+    private boolean checkForSQLWarnings(SQLWarning w) throws SQLException
     {
         boolean warning = false;
-        if(w != null) {
+        if(w != null)
+        {
             warning = true;
             System.out.println("\n**** Warning ****\n");
 
-            while(w != null) {
+            while(w != null)
+            {
                 System.out.println("SQLState: " + w.getSQLState());
                 System.out.println("Message:  " + w.getMessage());
                 System.out.println("Vendor:   " + w.getErrorCode());
@@ -60,15 +82,16 @@ public class Main {
     }
 
     /**
-     *	Close the connection to the data base source
+     * Close the connection to the data base source
      */
     public void close() throws Exception
     {
-        try {
+        try
+        {
             con.close();
             System.out.println("Test : Disconnecting ...");
-        }
-        catch(Exception e) {
+        } catch(Exception e)
+        {
             System.err.println("\n*** Exception caught in close()");
             throw e;
         }
@@ -78,7 +101,8 @@ public class Main {
 */
     private void printSQLErrors(SQLException e)
     {
-        while(e != null) {
+        while(e != null)
+        {
             System.err.println("SQLState: " + e.getSQLState());
             System.err.println("Message:  " + e.getMessage());
             System.err.println("Vendor:   " + e.getErrorCode());
@@ -87,29 +111,57 @@ public class Main {
         }
     }
 
-    private void creerTable() throws SQLException {
-
-        String creerTableCafe = "CREATE TABLE CAFE" +
-                "(NOM_CAFE VARCHAR(32)," +
-                " FO_ID INTEGER," +
-                " PRIX FLOAT," +
-                "VENTES INTEGER," +
-                " TOTAL INTEGER)";
-        String creerTableFournsseur = "CREATE TABLE FOURNISSEUR" +
-                "(FO_ID INTEGER," +
-                "NOM_FO VARCHAR(32)," +
-                "RUE VARCHAR(32)," +
-                "VILLE VARCHAR(32)," +
-                "ETAT VARCHAR(32)," +
-                "CODE_POSTALE VARCHAR(32))";
-
-
-        stmt.executeUpdate(creerTableCafe);
-        stmt.executeUpdate(creerTableFournsseur);
-
+    /**
+     * Méthode qui teste s'il y a des données dans une table
+     *
+     * @param table Nom de la table
+     * @return vrai s'il y a une table, faux sinon
+     * @throws SQLException
+     */
+    public boolean testExisteTables(String table) throws SQLException
+    {
+        DatabaseMetaData dbm = con.getMetaData();
+        // check if "employee" table is there
+        ResultSet tables = dbm.getTables(null, null, table, null);
+        if(tables.next())
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
     }
 
-    private void ajoutDonnees() throws SQLException {
+    /**
+     * Test si les tables sont crées et si non, les crées avec les données
+     * @throws SQLException
+     */
+    private void creerTable() throws SQLException
+    {
+        if(!testExisteTables("CAFE") && !testExisteTables("FOURNISSEUR"))
+        {
+            String creerTableCafe = "CREATE TABLE CAFE" +
+                    "(NOM_CAFE VARCHAR(32)," +
+                    " FO_ID INTEGER," +
+                    " PRIX FLOAT," +
+                    "VENTES INTEGER," +
+                    " TOTAL INTEGER)";
+            String creerTableFournsseur = "CREATE TABLE FOURNISSEUR" +
+                    "(FO_ID INTEGER," +
+                    "NOM_FO VARCHAR(32)," +
+                    "RUE VARCHAR(32)," +
+                    "VILLE VARCHAR(32)," +
+                    "ETAT VARCHAR(32)," +
+                    "CODE_POSTALE VARCHAR(32))";
+
+            stmt.executeUpdate(creerTableCafe);
+            stmt.executeUpdate(creerTableFournsseur);
+            this.ajoutDonnees();
+        }
+    }
+
+    private void ajoutDonnees() throws SQLException
+    {
         String insertCafe = "INSERT INTO CAFE " +
                 "VALUES ('Colombian', 101, 7.99, 0, 0), " +
                 " ('French_Roast', 49, 8.99, 0, 0) , " +
@@ -120,18 +172,25 @@ public class Main {
         stmt.executeUpdate(insertCafe);
     }
 
-    public static void main(String[] args) throws Exception {
-
-        String nomBD = "test";
-        String url = "jdbc:mysql://flatbrains.eu/" + nomBD;
-        String login = "NicoTPJDBC";
-        String mdp = "1234";
-
-        Main main = new Main(nomBD, url, login, mdp);
-        main.creerTable();
-        main.ajoutDonnees();
-        main.close();
-
-
+    /**
+     * SELECT * FROM CAFE
+     * @return
+     * @throws SQLException
+     */
+    public void requetesSelectCafe() throws SQLException
+    {
+        String res;
+        ResultSet rs =  stmt.executeQuery("SELECT * FROM CAFE");
+        System.out.println("NOM_CAFE\tFO_ID\tPRIX\tVENTES\tTOTAL");
+        while(rs.next())
+        {
+            res = "";
+            res += rs.getString("NOM_CAFE") + "\t";
+            res += rs.getString("FO_ID") + "\t";
+            res += rs.getString("PRIX") + "\t";
+            res += rs.getString("VENTES") + "\t";
+            res += rs.getString("TOTAL") + "\t";
+            System.out.println(res);
+        }
     }
 }
